22daa06ad404c970ecb03b2868256c3cd36ff8b4
AndreiS92 <andreisevcisen@gmail.com>
2023-06-12 03:04:41 +0200

    modificado Almacen

--- EuroXpress/src/Almacen.java
+++ EuroXpress/src/Almacen.java
@@ -1,2 +1,34 @@
+import java.util.ArrayList;
 public class Almacen {
+    private ArrayList<Producto> listadoProductos;
+
+    public Almacen() {
+        this.listadoProductos = new ArrayList<>();
+    }
+
+    public void agregarProducto(Producto producto) {
+        listadoProductos.add(producto);
+    }
+
+    public void eliminarProducto(Producto producto) {
+        listadoProductos.remove(producto);
+    }
+
+    public int obtenerStockProducto(Producto producto) {
+        int stock = 0;
+        for (Producto p : listadoProductos) {
+            if (p.getIdProducto().equals(producto.getIdProducto())) {
+                stock++;
+            }
+        }
+        return stock;
+    }
+
+    public ArrayList<Producto> getListadoProductos() {
+        return listadoProductos;
+    }
+
+    public void setListadoProductos(ArrayList<Producto> listadoProductos) {
+        this.listadoProductos = listadoProductos;
+    }
 }
